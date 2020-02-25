f47ee57fa4a3f174430543dd859ee5f88ec9ebc9
nguyen <thaonguyennguyen1197@gmail.com>
2020-02-25 22:23:22 +0700

    a

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,7 +4,7 @@
     <h1>Xin Chào Bách Khoa xấu xí</h1>
     <pre>
       <Component_1></Component_1>
-      <img src="https://i.pinimg.com/originals/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.png"/>
+      <img src="../assets/pika.png"/>
     </pre>
   </div>
 </template>
