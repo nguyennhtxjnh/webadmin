e5944d9569d137cd4d31e28e1030fc97e6565387
nguyen <thaonguyennguyen1197@gmail.com>
2020-02-25 22:18:40 +0700

    commit

---
+++ src/components/Component_1.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Thêm component 1 nè Khoa</h1>
+</template>
+
+<script>
+    export default {
+        name: "Component_1"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div>
+    <h1>Xin Chào Bách Khoa xấu xí</h1>
+    <pre>
+      <Component_1></Component_1>
+      <img src="https://i.pinimg.com/originals/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.png"/>
+    </pre>
+  </div>
+</template>
+
+<script>
+    import Component_1 from "@/components/Component_1";
+    export default {
+        name: "Welcome",
+      components: {Component_1},
+    }
+</script>
+
+<style scoped>
+
+</style>
